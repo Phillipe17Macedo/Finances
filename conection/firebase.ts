3ba8ea05ab1163ccc7b52620be1ddbcebf813fad
Phillipe <ph.stos.udia@gmail.com>
2024-04-09 22:05:39 -0300

    branch-config-conection-2b

--- conection/firebase.ts
+++ conection/firebase.ts
@@ -4,6 +4,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: "AIzaSyBgqGSo-2P2gSx5wIUzPeMgxWekXxrNh-k",
   authDomain: "finances-33298.firebaseapp.com",
+  databaseURL: "https://finances-33298-default-rtdb.firebaseio.com",
   projectId: "finances-33298",
   storageBucket: "finances-33298.appspot.com",
   messagingSenderId: "76100061458",
